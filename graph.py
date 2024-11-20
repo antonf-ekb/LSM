c583782ab4f6d55587e27c536838b4716040fde5
physlab <ngssoft@mail.ru>
2024-11-20 23:54:13 +0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -15,13 +15,13 @@ def fit_with_curve_fit(x, y):
 
 x=[None]*10
 y=[None]*10
-st.write("Введите 5 значений x")
+st.write("Введите 10 значений x")
 row1 = st.columns(10)
 for i, col in enumerate(row1):
 #on_change=capture_x_0,
     inp_key="x_"+str(i)
     x[i]=col.number_input("", key=inp_key)
-st.write("Введите 5 значений y")
+st.write("Введите 10 значений y")
 row2 = st.columns(10)
 for i, col in enumerate(row2):
     inp_key="y_"+str(i)
@@ -39,15 +39,17 @@ ax.tick_params(axis='both',which='both', direction="in", labelsize=10)
 #ax.yaxis.set_minor_locator(MultipleLocator(0.5))
 placeholder.pyplot(fig)
 if (st.button("Аппроксимировать прямой y=a+bx") and len(X[(X!=0) & (Y!=0)])>1):
-    popt, perr=fit_with_curve_fit(X[(X!=0) & (Y!=0)],Y[(X!=0) & (Y!=0)])
-    st.write("Результаты обработки с помощью МНК:")
-    st.write("b= "+ str(np.round(popt[0],3))) 
-    st.write("a= "+ str(np.round(popt[1],3)))    
-    st.write("$\delta_{b}$= "+ str(np.round(perr[0],3)))
-    st.write("$\delta_{a}$= "+ str(np.round(perr[1],3)))
-    X2=np.linspace(0,max(X),5)
-    ax.plot(X2,X2*popt[0]+popt[1], linestyle = '--', color="black")
-    placeholder.pyplot(fig)
-    st.write("Не забудьте округлить значения величин и их погрешности в соответствии с правилами округления, добавить единицы измерения а также названия осей на графике")
-#python -m streamlit run graph.py
-    #
+    if (len(X[(X!=0) & (Y!=0)])>2):
+        popt, perr=fit_with_curve_fit(X[(X!=0) & (Y!=0)],Y[(X!=0) & (Y!=0)])
+        st.write("Результаты обработки с помощью МНК:")
+        st.write("b= "+ str(np.round(popt[0],3))) 
+        st.write("a= "+ str(np.round(popt[1],3)))    
+        st.write("$\DELTA_{b}$= "+ str(np.round(perr[0],3)))
+        st.write("$\DELTA_{a}$= "+ str(np.round(perr[1],3)))
+        X2=np.linspace(0,max(X),5)
+        ax.plot(X2,X2*popt[0]+popt[1], linestyle = '--', color="black")
+        placeholder.pyplot(fig)
+        st.write("Не забудьте округлить значения величин и их погрешности в соответствии с правилами округления, добавить единицы измерения а также названия осей на графике")
+    else:
+        st.write("Введите не менее трех значений!")
+
